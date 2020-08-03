bc0b4ac98b8ce523196d601671d21c661e37946b
JACK Z X HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGJA14@OOCL.COM>
2020-08-03 22:24:14 +0800

    [Redux][Jack] refactor: failure to store each counter value and totalValue again

--- src/model/reducers/reducer.js
+++ src/model/reducers/reducer.js
@@ -1,22 +1,30 @@
 
 const initState = {
     state : {
+        size: 0,
         totalValue : 0,
         counters: []
     },
 }
 export default function updateState (state=initState.state, action) {
     const array = state.counters
-    const index = array.indexOf(action.counter)
+    console.log(array)
+    let index
     switch (action.type) {
       case "increment":
+        index = array.indexOf(action.counter)
         return array[index] + 1;
       case "decrement":
+        index = array.indexOf(action.counter)
         return array[index] - 1;
       case "addCounter":
-        return  array.push(action.counter);
+        array.push(action.counter);
+        return array.size() - 1;
+      case "gainCounterValue":
+          return array[action.key]
       default:
         return array[index] ;
+
     }
 };
 
